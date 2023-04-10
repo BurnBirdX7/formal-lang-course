568eab82d2ceceac6ed65bfbafcc7d1cb0181ba3
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-04-11 00:05:18 +0300

    Added method that returns dictionary of matrices

--- project/rfa.py
+++ project/rfa.py
@@ -2,6 +2,8 @@ from typing import Optional, Iterable, List
 from pyformlang.cfg import Variable
 from pyformlang.finite_automaton import DeterministicFiniteAutomaton
 
+from scipy.sparse import csr_matrix
+
 
 class RFABox:
     """
@@ -46,4 +48,37 @@ class RFA:
         self.boxes = boxes
 
     def minimize(self) -> "RFA":
+        """
+        Minimizes RFA by minimizing underlying boxes
+        """
         return RFA(self.start_symbol, [box.minimize() for box in self.boxes])
+
+    def get_matrices(self):
+        """
+        Returns adjacency matrices for the RFA
+        """
+        return {box.var: RFA.__dfa_get_matrix(box.dfa) for box in self.boxes}
+
+    @staticmethod
+    def __dfa_get_matrix(dfa: DeterministicFiniteAutomaton):
+        """
+        Returns adjacency matrices for single DFA
+        """
+        matrix = dict()
+        dfa_dict = dfa.to_dict()
+        states_len = len(dfa.final_states)
+
+        state_idx = {state: idx for idx, state in enumerate(dfa.states)}
+
+        for state_from, transition in dfa_dict.items():
+            for label, states_to in transition.items():
+                if not isinstance(states_to, set):
+                    states_to = {states_to}
+
+                for state_to in states_to:
+                    index_from = state_idx[state_from]
+                    index_to = state_idx[state_to]
+                    if label not in matrix:
+                        matrix[label] = csr_matrix((states_len, states_len), dtype=bool)
+                    matrix[label][index_from, index_to] = True
+        return matrix
