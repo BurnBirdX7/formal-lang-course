e6550b93e8d20c804e7ac00518f8b0609236e2d8
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-06-19 08:30:07 +0300

    [no ci] interactive mode for interpreter

--- project/__main__.py
+++ project/__main__.py
@@ -1,15 +1,49 @@
 import sys
 
-from project.language.interpret import execute_code
+from project.language.Executor import *
+from project.language.interpret import *
+
+from antlr4 import *
+
+
+def online():
+    typer = Typer()
+    executor = Executor(typer.variableTypes, typer.typeAnnotations, sys.stdout)
+    walker = ParseTreeWalker()
+
+    while True:
+        prog = input(" >>> ")
+        if prog == "q":
+            break
+
+        if not does_belong_to_language(prog):
+            print("Wrong syntax")
+            continue
+
+        tree = get_parser(prog)
+        program_tree = tree.program()
+
+        try:
+            walker.walk(typer, program_tree)
+        except ParseTypeError as e:
+            print("Type Error", file=sys.stderr)
+            print(e.value, file=sys.stderr)
+            continue
+
+        try:
+            executor.visit(program_tree)
+        except ExecutionError as e:
+            print("Execution error", file=sys.stderr)
+            print(e.value, file=sys.stderr)
 
 
 def main(argv):
     if len(argv) != 2:
-        print("Supply filename as an argument")
+        online()
+        return
 
     with open(argv[1], "r", encoding="utf-8") as f:
         prog = f.read()
-
     execute_code(prog, sys.stdout, sys.stderr)
 
 
